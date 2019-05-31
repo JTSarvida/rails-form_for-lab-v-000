37289813c8be26e418dc1fb2ec2b9a596c6dbf04
Joshua Tan <jtsarvida@gmail.com>
2019-05-31 18:14:11 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,7 +24,7 @@ class StudentsController < ApplicationController
   
   def update
     @student = Student.find(params[:id])
-    @student.update(params.require(:student).permit(:first_name, :last_name))
+    @student.update(student_params(:first_name, :last_name))
   end 
   
   private 
